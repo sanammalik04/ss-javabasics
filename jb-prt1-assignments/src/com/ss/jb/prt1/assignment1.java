7739c436978a9ae152d3ea7331d16c714b8aa360
github username <github email address>
2021-04-21 15:02:53 -0400

    Added folder for week one assignments

---
+++ jb-prt1-assignments/src/com/ss/jb/prt1/assignment1.java
@@ -0,0 +1,20 @@
+package com.ss.jb.prt1;
+/**
+ * 
+ */
+
+/**
+ * @author sanammalik
+ *
+ */
+public class assignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
