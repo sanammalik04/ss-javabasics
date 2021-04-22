300270a2a4e5a7a020b65487016001b7c3e513da
github username <github email address>
2021-04-22 13:55:17 -0400

    Completed star pattern assignment for week one and day one.

---
+++ jb-prt1-assignments/src/com/ss/jb/prt1/AssignmentOne.java
@@ -0,0 +1,96 @@
+package com.ss.jb.prt1;
+/**
+ * 
+ */
+
+/**
+ * @author sanammalik
+ *
+ */
+//First problem from the first assignment.
+
+public class AssignmentOne{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		patternPrintOne();
+		patternPrintTwo();
+		patternPrintThree();
+		patternPrintFour();
+	}
+	
+	public static void patternPrintOne() {
+		for(int row = 1; row <= 4; row++)
+		{
+			for(int column = 1; column <= row; column++)
+			{
+				
+				System.out.print("*");
+			}
+		
+			System.out.println();
+		}
+		System.out.println("- - - - - - - - - -");
+	}
+	
+	public static void patternPrintTwo() {
+		System.out.println("- - - - - - - - - -");
+		for(int row = 1; row <= 4; row++)
+		{
+			for(int column = 4; column >= row; column--)
+			{
+				
+				System.out.print("*");
+			}
+		
+			System.out.println();
+		}
+				
+}
+	public static void patternPrintThree() {
+		for(int row = 1; row <= 4; row++)
+		{
+			for(int blanks = 4; blanks >= row; blanks--)
+			{
+				System.out.print(" ");
+			}
+			for(int columnOne = 1; columnOne <= row; columnOne++)
+			{
+				System.out.print("*");
+			}
+			for(int columnTwo = 2; columnTwo <= row; columnTwo++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("- - - - - - - - - - -");
+	}
+
+	public static void patternPrintFour() {
+		System.out.println("- - - - - - - - - - - -");
+		for(int row = 1; row <= 4; row ++)
+		{
+			for(int blanks = 1; blanks <= row; blanks++)
+			{
+				System.out.print(" ");
+			}
+			for(int columnOne = 4; columnOne >= row; columnOne--)
+			{
+				System.out.print("*");
+			}
+			for(int columnTwo = 3; columnTwo >= row; columnTwo--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
+
+	
+
+		
