56d7d32cb7fd986985da7f64719d4ef4616ed42a
sanammalik04 <sanam.malik04@gmail.com>
2021-04-28 15:19:36 -0400

    found the coordinates of the max value and completed assignment

--- jb-prt1-assignments/src/com/ss/jb/prt2/AssignmentsPrt2.java
+++ jb-prt1-assignments/src/com/ss/jb/prt2/AssignmentsPrt2.java
@@ -3,6 +3,9 @@
  */
 package com.ss.jb.prt2;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author sanammalik
  *
@@ -18,9 +21,11 @@ public class AssignmentsPrt2 {
 	}
 	
 	public static void maxNumTwoDArr() {
-		int [][] arr= { {5, 10, 15, 20}, {25, 30, 35, 40}, {45, 50, 60, 70, 75} };
+		int [][] arr= { {5, 10, 15, 50}, {25, 30, 35, 40} };
 		
 		int max = arr[0][0];
+		int x = 0;
+		int y = 0;
 		
 		for(int i=0; i<arr.length; i++)
 		{
@@ -29,14 +34,14 @@ public class AssignmentsPrt2 {
 				if(arr[i][j] > max)
 				{
 					max = arr[i][j];
+					x = i;
+					y = j;	
 				}
+				
 			}
 //			
 		}
-		System.out.println(max);
-		
-		
-		
+		System.out.println("The max value is: " + Integer.toString(max) + ", and the coordinates of the max value are: (" + Integer.toString(x) + ", " + Integer.toString(y) + ").");
 		
 	}
 
